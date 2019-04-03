a495c73d8d9125c6184ac479a7a55bcb78f4e8f3
jeonzi <jieun5230@naver.com>
2019-04-04 01:56:31 +0900

    creating GraphL Sever

--- src/server.js
+++ src/server.js
@@ -1,1 +1,25 @@
-console.log("hello");
+require("dotenv").config();
+import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+
+const PORT = process.env.PORT || 4000;
+
+// type 정의와 resolver가 필요하다.
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`;
+
+const resolvers = {
+  Query: { hello: () => "Hi Jieun" }
+};
+
+// GraphQLServer에는 express서버가 내장되어 있다.
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.express.use(logger("dev"));
+
+server.start({ port: PORT }, () =>
+  console.log(`Server Running on port ${PORT}`)
+);
